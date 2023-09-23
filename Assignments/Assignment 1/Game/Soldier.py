cb067dff7542bdf7b6ed00c45890b09a7fa51c9e
BungholeXD <alexgasee03@gmail.com>
2023-09-23 17:42:57 -0400

    Shop Logic + Next Quest Logic

--- Assignments/Assignment 1/Game/Soldier.py
+++ Assignments/Assignment 1/Game/Soldier.py
@@ -6,13 +6,14 @@ class Soldier:
     def __init__(self, name):
         #Initializing the attributes of the soldier
         self.name = name
-        self.gold = 0
+        self.gold = 10
         self.health = 50
         self.damage = 2
         self.armor = 1
         self.stamina = 1
         self.questCompleted = False
-        self.shopVisited = False
+        self.shopVisited = True
+        self.questsCompletedCount = 0
         #Dictionary of items that the soldier has
         self.items = {"Offensive": [], "Defensive": []}
         #List of challenges that the soldier has to complete
@@ -33,73 +34,172 @@ class Soldier:
             }
         }
         
-        def battleDragon(self):
-            #Creating the logic for the first challenge, "Battle your first dragon"
-            print("While returning from a long journey, you encounter a dragon!")
+    def battleDragon(self):
+        #Creating the logic for the first challenge, "Battle your first dragon"
+        print("While returning from a long journey, you encounter a dragon!")
             
-            dragonHealth = 30
-            dragon_damage = random.randint(2,12)
+        dragonHealth = 30
+        dragonDamage = random.randint(2,12)
             
-            while True: #Creating a loop that will run until you choose to battle the dragon
-                dragonChoice = input("Do you want to battle the dragon? (y/n): ").lower()
+        while True: #Creating a loop that will run until you choose to battle the dragon
+            dragonChoice = input("Do you want to battle the dragon? (y/n): ").lower()
                 
-                if dragonChoice in {'y','yes'}:
-                    print("You have chosen to battle the dragon!")
-                    while self.health > 0 and dragonHealth > 0:
-                        print("You angered the dragon, and it attacks you!")
+            if dragonChoice in {'y','yes'}:
+                print("You have chosen to battle the dragon!")
+                while self.health > 0 and dragonHealth > 0:
+                    print("You angered the dragon, and it attacks you!")
                     
-                    print("Luckily You dodged its attack, what will you do now?")
-                    print(f"Your health: {self.health}, Dragon's health: {dragonHealth}")
+                print("Luckily You dodged its attack, what will you do now?")
+                print(f"Your health: {self.health}, Dragon's health: {dragonHealth}")
 
-                    choice = input("Do you want to 1) Attack or 2) Defend")
-                    die1 = random.randint(1,6)
-                    die2 = random.randint(1,6)
-                    numRolled = die1 + die2
-                    criticalStrike = die1 == die2
+                choice = input("Do you want to 1) Attack or 2) Defend")
+                die1 = random.randint(1,6)
+                die2 = random.randint(1,6)
+                numRolled = die1 + die2
+                criticalStrike = die1 == die2
                     
-                    if criticalStrike:
-                        print("Critical Strike!")
+                if criticalStrike:
+                    print("Critical Strike!")
                         
-                    if choice == '1': #Attack
-                        print(f"You decide to attackthe dragon!, you rolled a {numRolled}")
-                        dragonAction = random.choice(['dodge', 'defend'])
+                if choice == '1': #Attack
+                    print(f"You decide to attackthe dragon!, you rolled a {numRolled}")
+                    dragonAction = random.choice(['dodge', 'defend'])
                         
-                        if dragonAction == 'dodge':
-                            if numRolled < 6:
-                                print("The Dragon dodged your attack!")
-                            else:
-                                print("You hit the dragon!")
-                                damage = numRolled + (2 if criticalStrike else 0)
-                                dragonHealth -= damage
+                    if dragonAction == 'dodge':
+                        if numRolled < 6:
+                            print("The Dragon dodged your attack!")
                         else:
-                            print("The dragon defended your attack!, You will deal reduced damage")
-                            damage = numRolled + (2 if criticalStrike else 0) - 1
-                            dragonHealth -=  max(damage - dragonHealth, 0)
+                            print("You hit the dragon!")
+                            damage = numRolled + (2 if criticalStrike else 0)
+                            dragonHealth -= damage
+                    else:
+                        print("The dragon defended your attack!, You will deal reduced damage")
+                        damage = numRolled + (2 if criticalStrike else 0) - 1
+                        dragonHealth -=  max(damage - dragonHealth, 0)
                     
-                    elif choice == '2': #Defend
-                        print("You decide to defend against the dragon!")
-                        dragonAttack = random.randint(1,6) + random.randint(1,6)
-                        if dragonAttack > 6:
-                            print("The dragon hit you!")
-                            damage = dragonAttack - self.armor
-                            self.health -= max(damage - self.health, 0)
-                        else:
-                            print("The dragon missed!")
+                elif choice == '2': #Defend
+                    print("You decide to defend against the dragon!")
+                    dragonAttack = random.randint(1,6) + random.randint(1,6)
+                    if dragonAttack > 6:
+                        print("The dragon hit you!")
+                        damage = dragonAttack - self.armor
+                        self.health -= max(damage - self.health, 0)
+                    else:
+                        print("The dragon missed!")
                             
-                    if dragonHealth <= 0:
-                        print("You have defeated the dragon!")
-                        self.challenges.remove({"name": "Battle your first dragon", "attribute": "damage"})
-                        self.damage += 2
-                        self.gold += 30
-                        break
-                        print("You have gained 30 gold!")
-                    elif self.health <= 0:
-                        print("You have been defeated by the dragon!")
-                        print("GAME OVER")
-                        break
-                        
+                if dragonHealth <= 0:
+                    print("You have defeated the dragon!")
+                    self.challenges.remove({"name": "Battle your first dragon", "attribute": "damage"})
+                    self.damage += 2
+                    self.gold += 30
+                    print("You have gained 30 gold!")
+                    self.questCompleted = True
+                    self.shopVisited = False
+                    self.questsCompletedCount += 1
                     break
-                elif dragonChoice in {'n','no'}:
-                    print("You remember the kings words, \"I became king because I never backed down from a challenge\"")
+                elif self.health <= 0:
+                    print("You have been defeated by the dragon!")
+                    print("GAME OVER")
+                    break
+                        
+                break
+            elif dragonChoice in {'n','no'}:
+                print("You remember the kings words, \"I became king because I never backed down from a challenge\"")
+            else:
+                print("Invalid Input. Please Enter 'y', 'yes', 'n', or 'no'")
+    
+    def invadeKingdom(self):
+        if 1 > 3:
+            print("You have not completed the previous challenges yet!")
+            return
+        
+    
+    def overthrowKing(self):
+        if 1 > 2:
+            print("You have not completed the previous challenges yet!")
+            return
+        
+    
+    def becomeKing(self):
+        if 1 > 1000
+            print("You have not completed the previous challenges yet!")
+            return
+        
+    def visitShop(self):
+        if self.quests_completed_count <= 0 or (self.quests_completed_count <= len(self.challenges) and self.shop_visited):
+            print("You cant visit the shop now!")
+            return
+        
+        while True:
+            print("\nWelcome to the shop!")
+            print("You Currently Have:", self.gold, "gold")
+            print("\nAvailable Items:")
+                
+            print("Offensive Items:")
+            for item, cost in self.shopItems["Offensive"].items():
+                print(f"{item}: {cost} gold")
+                
+            print("\nDefensive Items:")
+            for item, cost in self.shopItems["Defensive"].items():
+                print(f"{item}: {cost} gold")
+                
+            itemToBuy = input("\nWhat would you like to buy? (Type 'exit' to exit): ").title()
+            
+            if itemToBuy == 'exit':
+                break
+            
+            foundItem = None
+            itemType = None
+            
+            #Checking if the item exists in the shop
+            for itype, items in self.shopItems.items():
+                if itemToBuy in items:
+                    foundItem = itemToBuy
+                    itemType = itype
+                    break
+            
+            if foundItem:
+                if self.gold >= self.shopItems[itemType][foundItem]:
+                    #deduct the gold from the player and then add the item to the player's inventory
+                    self.gold = self.gold - self.shopItems[itemType][foundItem]
+                    self.items[itemType].append(foundItem)
+                    print(f"You have bought {foundItem} for {self.shopItems[itemType][foundItem]} gold!")
                 else:
-                    print("Invalid Input. Please Enter 'y', 'yes', 'n', or 'no'")
+                    print("You don't have enough gold!")
+            else:
+                print("Invalid item name. Please type the exact name of the item!")
+            
+            self.shopVisited = True
+
+    def startNextQuest(self):
+        if self.questsCompletedCount == 0:
+            print("You have not completed any quests yet!")
+            #if no quests are completed the player is then able to start the first quest
+            self.battleDragon() #starting the first quest
+            
+        elif self.challenges: #checking if there are still challenges remaining
+            challenge = self.challenges[0] # getting the next challenge
+            challengeName = challenge["name"]
+            print(f"\nStarting quest: {challengeName}")
+            
+            #Checking if the player is allowed to visit the shop before starting the quest
+            if not self.shopVisited and self.questsCompletedCount > 0:
+                self.visitShop()
+            
+            
+            #Adding the logic for the next quests
+            if challengeName == "Invade your first kingdom":
+                self.invadeKingdom()
+            elif challengeName == "Overthrow the evil king":
+                self.overthrowKing()
+            elif challengeName == "Become The King":
+                self.becomeKing()
+                
+            
+            #Update questsCompleteCount and reseting the value of shopVisited after completing a quest
+            self.questsCompletedCount += 1
+            self.shopVisited = False
+            
+        else:
+            print("You have completed all the quests!, You are the new king!")
+
