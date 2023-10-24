7461e41adf78bf09fcc934a2d5ca14209406f0e4
1v9alex <alexgasee03@gmail.com>
2023-10-24 12:37:37 -0400

    added check ti make sure stock never goes neg

--- Assignments/Assignment 2/Product.py
+++ Assignments/Assignment 2/Product.py
@@ -11,11 +11,18 @@ class Product:
     
     
     def monthlySimuation(self):
-        unitsSold = random.randint(self.monthlyUnits-10,self.monthlyUnits+10)
-        #generating a predicted stock statement for 12 months
-        self.stockLevel = self.stockLevel + self.monthlyUnits - unitsSold
+        unitsSold = random.randint(self.monthlyUnits-10, self.monthlyUnits+10)
+        # Check if unitsSold exceeds available stock
+        available_stock = self.stockLevel + self.monthlyUnits
+        if unitsSold > available_stock:
+            unitsSold = available_stock
+        # Update the stock level
+        self.stockLevel = available_stock - unitsSold
         return unitsSold
+
     
     
     def netProfit(self,totalSoldUnits):
-        return (totalSoldUnits * self.salePrice) - (self.monthlyUnits * self.manufactureCost)
+        totalRevenue = totalSoldUnits * self.salePrice
+        totalManufacturingCost = self.monthlyUnits * 12 * self.manufactureCost
+        return totalRevenue - totalManufacturingCost
