3c5aee516d3300cc44ec86ac56b0cb5abea478bf
1v9alex <alexgasee03@gmail.com>
2023-11-20 15:32:10 -0500

    showMainMenu Logic

--- Assignments/Assignment 3/Program.py
+++ Assignments/Assignment 3/Program.py
@@ -5,8 +5,20 @@ class Program:
         self.bank = bank
     
     def showMainMenu(self):
-        #TODO: Implement this method
-        pass
+        while True:
+            print("\n1. Open Account")
+            print("2. Select Account")
+            print("3. Exit")
+            choice = input("Enter your choice: ")
+            if choice == "1" or choice == "Open Account":
+                self.openAccount()
+            elif choice == "2" or choice == "Select Account":
+                self.selectAccount()
+            elif choice == "3" or choice == "Exit":
+                print("Thank you for using our banking system.")
+                break
+            else:
+                print("Invalid choice. Please try again.")
     
     def showAccountMenu(self,account):
         #TODO: Implement this method
