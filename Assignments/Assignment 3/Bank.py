bac00c83a2803d3874e088abeb8030a7e68da8fa
1v9alex <alexgasee03@gmail.com>
2023-11-20 16:35:50 -0500

    Fixes, it now works as intended

--- Assignments/Assignment 3/Bank.py
+++ Assignments/Assignment 3/Bank.py
@@ -3,7 +3,17 @@ class Bank:
     def __init__(self,bankName):
         self._bankName = bankName
         self._accounts = []
+        #Creating 5 hardcoded accounts and appending them to the accounts list
+        self._accounts.append(SavingsAccount("12345", "John Doe", 0.02, 10000.0, 500.0))
+        self._accounts.append(SavingsAccount("12346", "Jane Smith", 0.03, 15000.0, 1000.0))
+        self._accounts.append(ChequingAccount("12347", "Alice Brown", 0.01, 5000.0, 1000.0))
+        self._accounts.append(ChequingAccount("12348", "Bob Martin", 0.01, 3000.0, 500.0))
+        self._accounts.append(SavingsAccount("12349", "Charlie Johnson", 0.04, 20000.0, 2000.0))
     
+    
+    
+
+        
     def openAccount(self, accountType, accountNumber, accountHolderName, rateOfInterest, currentBalance, additionalParam):
         if accountType.lower() == "savings":
             # For SavingsAccount, additionalParam is assumed to be minimum balance
