b283ec8da43e4b7b40338b52d95dacf35c95c7b2
1v9alex <alexgasee03@gmail.com>
2023-11-20 15:19:50 -0500

    skeleton classes main and bank

---
+++ Assignments/Assignment 3/Bank.py
@@ -0,0 +1,12 @@
+class Bank:
+    def __init__(self,bankName):
+        self._bankName = bankName
+        self._accounts = []
+    
+    def openAccount(self,accountType,*args):
+        #TODO: Implement this method
+        pass
+    
+    def searchAccount(self,accountNumber):
+        #TODO implement this method
+        pass
---
+++ Assignments/Assignment 3/Program.py
@@ -0,0 +1,19 @@
+from Bank import Bank
+
+class Program:
+    def __init__(self,bank):
+        self.bank = bank
+    
+    def showMainMenu(self):
+        #TODO: Implement this method
+        pass
+    
+    def showAccountMenu(self,account):
+        #TODO: Implement this method
+        pass
+    
+    def run(self):
+        #TODO implement this method
+        pass
+    
+    
