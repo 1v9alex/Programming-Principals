928a98d40d3e6338b232f2bfc7abe986e9c21ae3
1v9alex <alexgasee03@gmail.com>
2023-11-20 15:29:37 -0500

    bank logic + add import

--- Assignments/Assignment 3/Bank.py
+++ Assignments/Assignment 3/Bank.py
@@ -1,12 +1,21 @@
+from Account import SavingsAccount, ChequingAccount
 class Bank:
     def __init__(self,bankName):
         self._bankName = bankName
         self._accounts = []
     
     def openAccount(self,accountType,*args):
-        #TODO: Implement this method
-        pass
+        if accountType == "Savings":
+            account = SavingsAccount(*args)
+        elif accountType == "Chequing":
+            account = ChequingAccount(*args)
+        else:
+            raise ValueError("Invalid account type.")
+        self._accounts.append(account)
+        return account
     
     def searchAccount(self,accountNumber):
-        #TODO implement this method
-        pass
+        for account in self._accounts:
+            if account.getAccountNumber() == accountNumber:
+                return account
+        return None
