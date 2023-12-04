17cdb857a61a823627913474a42c6822662ebbd4
1v9alex <alexgasee03@gmail.com>
2023-12-04 12:42:44 -0500

    Class for creating champion objects + csv

---
+++ Assignments/Assignment 4/Champion.py
@@ -0,0 +1,31 @@
+class Champion:
+    def __init__(self,name,tier,difficulty,role):
+        self.name = name
+        self.tier = tier
+        self.difficulty = difficulty
+        self.role = role
+    
+    #Getter and Setter Methods for the Champion Class
+    def GetName(self):
+        return self.name
+    
+    def setName(self,name):
+        self.name = name
+    
+    def GetTier(self):
+        return self.tier
+    
+    def setTier(self,tier):
+        self.tier = tier
+        
+    def GetDifficulty(self):
+        return self.difficulty
+    
+    def setDifficulty(self,difficulty):
+        self.difficulty = difficulty
+        
+    def GetRole(self):
+        return self.role
+    
+    def setRole(self,role):
+        self.role = role
