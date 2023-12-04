0ef927ec8c8e484400d00e8f752878b68ac191bd
1v9alex <alexgasee03@gmail.com>
2023-12-04 13:27:30 -0500

    fix input bug

--- Assignments/Assignment 4/MainApplication.py
+++ Assignments/Assignment 4/MainApplication.py
@@ -14,11 +14,11 @@ def main():
         ui.displayMenu()
         choice = ui.getUserInput()
         
-        if choice == '1':
+        if choice == 1:
             name,tier,difficulty,role = ui.getChampionData()
             manager.addChampion(Champion(name,tier,difficulty,role))
             print("Champion added successfully.")
-        elif choice == '2':
+        elif choice == 2:
             searchChoice = ui.getSearchCriteria()
             query = ui.getSearchQuery(["Name","Tier","Difficulty","Role"][searchChoice-1])
             if searchChoice == 1:
@@ -30,17 +30,17 @@ def main():
             elif searchChoice == 4:
                 results = manager.searchByRole(query)
             ui.displaySearchResults(results)
-        elif choice == '3':
+        elif choice == 3:
             name = input("Enter the name of the champion you want to update: ")
             newData = ui.getChampionData()[1:]
             manager.updateChampion(name, {'tier': newData[0], 'difficulty': newData[1], 'role': newData[2]})
-        elif choice == '4':
+        elif choice == 4:
             name = input("Enter the name of the champion you want to delete: ")
             manager.deleteChampion(name)
-        elif choice == '5':
+        elif choice == 5:
             champions = manager.getAllChampions()
             ui.displayAllChampions(champions)
-        elif choice == '6':
+        elif choice == 6:
             break
         else:
             print("Please enter a valid choice.")
