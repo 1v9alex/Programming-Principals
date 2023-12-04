3788b7aba69214b7cc792be05c73acf995db1282
1v9alex <alexgasee03@gmail.com>
2023-12-04 13:03:48 -0500

    added reading and writing data to the csv

---
+++ Assignments/Assignment 4/DataPersistenceManager.py
@@ -0,0 +1,28 @@
+import csv
+from Champion import Champion
+
+class DataPersistenceManager:
+    def loadData(self,fileName):
+        champions = []
+        try:
+            with open(fileName, mode='r') as file:
+                reader = csv.reader(file)
+                for row in reader:
+                    champions.append(Champion(row['Champion Name'], row['Tier'], row['Difficulty'], row['Role']))
+        except Exception as e:
+            print("Error loading data:", e)
+        return champions
+    
+    
+    def saveData(self,champions,fileName):
+        try:
+            with open(fileName, mode='w',newline='') as file:
+                fieldnames = ['Champion Name', 'Tier', 'Difficulty', 'Role']
+                writer = csv.DictWriter(file, fieldnames=fieldnames)
+                
+                writer.writeheader()
+                for champ in champions:
+                    writer.writerow({'Champion Name': champ.get_name(), 'Tier': champ.get_tier(), 'Difficulty': champ.get_difficulty(), 'Role': champ.get_role()})
+        except Exception as e:
+            print("Error saving data:", e)
+
