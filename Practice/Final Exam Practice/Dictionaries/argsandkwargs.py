88fb4b324e720b8819eca46dbc9c8ae39d8410ca
1v9alex <alexgasee03@gmail.com>
2023-12-10 13:22:34 -0500

    args + kwargs practice

---
+++ Practice/Final Exam Practice/Dictionaries/argsandkwargs.py
@@ -0,0 +1,45 @@
+carComapny = ['Audi','BMW','Ferrari']
+
+print(*carComapny)
+
+techStackOne = {"React":"Facebook","Angular":"google","dotNet":"Microsoft"}
+techStackTwo = {"dotNet": "Microsoft","Java":"Minecraft"}
+
+mergedStack = {**techStackOne, **techStackTwo}
+
+print(mergedStack)
+
+print(*techStackOne)
+
+
+carComapny2 = [["Audi","R8"],["BMW","Car"]]
+
+carCompany3 = ["Lamborgini","porsche"]
+print(*carComapny2)
+
+
+techStackOne.update(techStackTwo)
+
+print(techStackOne)
+
+v = techStackOne["Angular"]
+
+techStackOne["Angular"] = "Not google"
+
+print(techStackOne)
+
+def makeSentence(**words):
+    sentence = ''
+    for word in words.values():
+        sentence += word
+    return sentence
+
+print('Sentence:',makeSentence(a='kwargs ',b="are ",c="awesome"))
+
+def whatTechTheyUse(**kwargs):
+    result = []
+    for key,value in kwargs.items():
+        result.append(f"{key} uses {value}")
+    return result
+
+print(whatTechTheyUse(Google="Angular",Facebook='react',Microsoft=".NET"))
