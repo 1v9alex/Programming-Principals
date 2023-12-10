89fbb4522e776d596d3b84781da70a7cc39d6ac4
1v9alex <alexgasee03@gmail.com>
2023-12-10 17:46:39 -0500

    more file test

--- Practice/Final Exam Practice/Files/Practice 1/Prac1.py
+++ Practice/Final Exam Practice/Files/Practice 1/Prac1.py
@@ -1,6 +1,18 @@
 f = open("Practice\Final Exam Practice\Files\Practice 1\data.txt", "r")
 
 for line in f:
-    values = line.split()
-    print(values[0])
+    # Split the line into parts
+    parts = line.split()
+
+    # The name is the first part and the marks are the rest
+    name = parts[0]
+    marks = parts[1:]
+
+    # Check if the student has 6 or more marks
+    scores = [int(mark) for mark in marks]
+    
+    avgScore = sum(scores) / len(scores)
+    
+    
+    print(round(avgScore,2))
 f.close()
